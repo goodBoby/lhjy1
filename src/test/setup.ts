31c5b0e115fac22ef5c4d64469e5acf619ee8471
Developer <developer@chinamediguide.com>
2026-03-11 08:17:50 +0800

    fix: Fix vitest import and add to dependencies

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 import { cleanup } from '@testing-library/react'
-import { afterEach, vi } from 'viest'
+import { afterEach, vi } from 'vitest'
 
 // Cleanup after each test
 afterEach(() => {
